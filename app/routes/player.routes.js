b06167fd668087e2624953ae3da3e019e1822a35
prasetion <prasetion@gmail.com>
2021-10-08 22:41:08 +0700

    adding swagger

--- app/routes/player.routes.js
+++ app/routes/player.routes.js
@@ -1,4 +1,4 @@
-module.exports = app => {
+module.exports = (app) => {
   const players = require("../controllers/player.controller.js");
 
   var router = require("express").Router();
@@ -10,6 +10,11 @@ module.exports = app => {
   router.put("/players/:id", players.update);
   router.post("/players/exp/:id", players.getExperience);
   router.delete("/players/:id", players.delete);
+
+  // swagger endpoints
+  const swaggerJSON = require("../swagger.json");
+  const swaggerUI = require("swagger-ui-express");
+  app.use("/api/docs", swaggerUI.serve, swaggerUI.setup(swaggerJSON));
 
   // API prefix
   app.use("/api", router);
